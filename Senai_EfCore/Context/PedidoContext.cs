e3fc0eabd644743ebd3bf5783b35dfe477242fbc
unknown <fe97964@gmail.com>
2020-09-09 00:29:26 -0300

    Primeiro Commit

---
+++ Senai_EfCore/Context/PedidoContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Senai_EfCore.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai_EfCore.Context
+{
+    public class PedidoContext : DbContext
+    {
+        public DbSet<Pedido> Pedido { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<PedidoItem> PedidosItens { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+                optionsBuilder.UseSqlServer(@"Data source=\SQLEXPRESS; Initial Catalog=Db_Senai_Pedidos_Dev; user ID = sa;Password=sa@132");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ Senai_EfCore/Domains/Produto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai_EfCore.Domains
+{
+    /// <summary>
+    /// Define a classe produto
+    /// </summary>
+    public class Produto
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public float Preco { get; set; }
+
+        public Produto()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
