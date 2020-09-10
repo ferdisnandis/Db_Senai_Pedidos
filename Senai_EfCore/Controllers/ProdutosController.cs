822d57b37f44bf24e6c139798dad0fdf884567c4
unknown <fe97964@gmail.com>
2020-09-09 21:58:52 -0300

    Controller feito

---
+++ Senai_EfCore/Controllers/ProdutosController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Senai_EfCore.Domains;
+using Senai_EfCore.Interfaces;
+using Senai_EfCore.Repositories;
+
+namespace Senai_EfCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private IProdutoRepository _produtoRepository;
+
+        public ProdutosController()
+        {
+            _produtoRepository = new ProdutoRepository();
+        }
+
+        [HttpGet]
+        public List<Produto> Get()
+        {
+            return _produtoRepository.Listar();
+        }
+
+        [HttpGet("{id}")]
+        public Produto Get(Guid id)
+        {
+            return (Produto)_produtoRepository.BuscarPorId(id);
+        }
+
+        [HttpPost]
+        public void Post(Produto produto)
+        {
+            _produtoRepository.Adicionar(produto);
+        }
+
+        [HttpPut("{id}")]
+        public void Put(Guid id, Produto produto)
+        {
+            produto.Id = id;
+            _produtoRepository.Editar(produto);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(Guid id)
+        {
+            _produtoRepository.Remover(id);
+        }
+    }
+}
